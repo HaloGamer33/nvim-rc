148361870ebb35ded614f3c6bc9be3af1adad457
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2024-03-19 17:37:39 -0600

    feat: add auto centering to half page movments

--- lua/base-config/mappings.lua
+++ lua/base-config/mappings.lua
@@ -8,12 +8,17 @@ set("i", "<C-H>", "<C-W>")                             -- "ctrl + backspace" to 
 
 set("n", "<C-j>", "<C-d>zz") -- Moving half page but keeping cursor center
 set("n", "<C-k>", "<C-u>zz")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-k>", "<C-u>zz")
 
 set("v", "K", ":m '<-2<CR>gv=gv") -- "K" in visual mode will move selection up
 set("v", "J", ":m '>+1<CR>gv=gv") -- "J" in visual mode will move selection down
 
-set("v", "n", "nzzzv") -- Centering cursor when searching
+set("v", "n", "nzzzv") -- Centering cursor when searching - Visual Mode
 set("v", "N", "Nzzzv")
+
+set("n", "n", "nzz") -- Centering cursor when searching - Visual Mode
+set("n", "N", "Nzz")
 
 set({"n", "v"}, "<leader>y", '"+y')
 
