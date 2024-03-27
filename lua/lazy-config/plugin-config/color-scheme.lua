8c221983cb1a0da34434a2eb99b8c53d1b1a27ec
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2024-03-27 17:29:46 -0600

    feat: update tokyonight preferences

--- lua/lazy-config/plugin-config/color-scheme.lua
+++ lua/lazy-config/plugin-config/color-scheme.lua
@@ -1,7 +1,7 @@
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
@@ -34,6 +34,6 @@ require("tokyonight").setup({
   on_highlights = function(highlights, colors) end,
 })
 
-vim.cmd.colorscheme("tokyonight-moon")
+vim.cmd.colorscheme("tokyonight")
 
 vim.opt.fillchars = { eob = " " }
