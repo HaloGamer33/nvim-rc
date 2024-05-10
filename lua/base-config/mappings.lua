87f89a309f376cf9d70e368f5412ba4ab39629e5
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2024-05-10 13:41:13 -0600

    feat: remove not needed remap for half page jumps

--- lua/base-config/mappings.lua
+++ lua/base-config/mappings.lua
@@ -6,10 +6,11 @@ vim.g.mapleader = " " -- Mapping leader to space.
 
 set("i", "<C-H>", "<C-W>") -- "ctrl + backspace" to delete whole word in insert mode.
 
-set("n", "<C-j>", "<C-d>zz") -- Moving half page but keeping cursor center
-set("n", "<C-k>", "<C-u>zz")
-set("n", "<C-d>", "<C-d>zz")
-set("n", "<C-k>", "<C-u>zz")
+-- No longer necessary when using scrolloff setting
+-- set("n", "<C-j>", "<C-d>zz") -- Moving half page but keeping cursor center
+-- set("n", "<C-k>", "<C-u>zz")
+-- set("n", "<C-d>", "<C-d>zz")
+-- set("n", "<C-k>", "<C-u>zz")
 
 set("v", "K", ":m '<-2<CR>gv=gv") -- "K" in visual mode will move selection up
 set("v", "J", ":m '>+1<CR>gv=gv") -- "J" in visual mode will move selection down
