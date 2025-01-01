c647aa3b0e8a4d02fef3bce855e6262c99f9fb8a
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2024-12-31 18:10:32 -0600

    refactor: rename file-tree.lua to file-explorer.lua

---
+++ lua/lazy-config/plugin-config/file-explorer.lua
@@ -0,0 +1,5 @@
+-- For neo-tree.nvim
+-- vim.keymap.set('n', '<leader>`', '<cmd>Neotree toggle<cr>')   -- 'Leader + `' to open file tree.
+
+-- For nvim-tree.lua
+vim.keymap.set('n', '<leader>~', '<cmd>NvimTreeToggle<cr>')
