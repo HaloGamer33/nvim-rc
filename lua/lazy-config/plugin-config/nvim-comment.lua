c362191bb5368b98362cdc20e3d40e624770022f
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-01-03 23:40:28 -0600

    feat: add configuration to nvim-comment

--- lua/lazy-config/plugin-config/nvim-comment.lua
+++ lua/lazy-config/plugin-config/nvim-comment.lua
@@ -1,1 +1,10 @@
-require("nvim_comment").setup{}
+require("nvim_comment").setup {
+  -- should comment out empty or whitespace only lines
+  comment_empty = false,
+  -- trim empty comment whitespace
+  comment_empty_trim_whitespace = true,
+  -- Normal mode mapping left hand side
+  line_mapping = "gcc",
+  -- Visual/Operator mapping left hand side
+  operator_mapping = "gc",
+}
