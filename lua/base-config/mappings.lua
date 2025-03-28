2dd9d01fcba18507a8b8e29084145a88fae7b79b
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-03-28 05:14:07 -0600

    feat: add lsp refactor binding

--- lua/base-config/mappings.lua
+++ lua/base-config/mappings.lua
@@ -6,13 +6,16 @@ vim.g.mapleader = ' ' -- Mapping leader to space.
 
 set({'i', 'c'}, '<C-H>', '<C-W>') -- 'ctrl + backspace' to delete whole word in insert mode.
 
-set('v', 'K', ":m '<-2<CR>gv=gv") -- 'K' in visual mode will move selection up
-set('v', 'J', ":m '>+1<CR>gv=gv") -- 'J' in visual mode will move selection down
+set('v', 'K', ":m '<-2<CR>gv=gv") -- 'K' in visual mode will move selection up.
+set('v', 'J', ":m '>+1<CR>gv=gv") -- 'J' in visual mode will move selection down.
 
--- Use visual lines to move up and down
+-- Use visual lines to move up and down.
 set({'n', 'v'}, 'j', 'gj')
 set({'n', 'v'}, 'k', 'gk')
 
--- Yanking and pasting into/from the system clipboard
+-- Yanking and pasting into/from the system clipboard.
 set({'n', 'v'}, '<leader>y', '"+y')
 set({'n', 'v'}, '<leader>p', '"+p')
+
+-- Refactor with the help of the LSP.
+set('n', '<leader>rn', vim.lsp.buf.rename)
