51d77ef3c95d772062beac4e6ab2a9b878522567
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-03-28 04:31:07 -0600

    fix: correct how treesitter modules were being configured

--- lua/lazy-config/plugin-config/treesitter.lua
+++ lua/lazy-config/plugin-config/treesitter.lua
@@ -8,21 +8,17 @@ require'nvim-treesitter.configs'.setup {
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
 
-    modules = {
-        indent = {
-            enable = false,
-            disable = {'xml'},
-        },
+    indent = {
+        enable = true,
     },
-
     highlight = {
         enable = true,
-        disable = {'xml'},
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        -- additional_vim_regex_highlighting = false,
+    },
+    folding = {
+        enable = true,
+    },
+
+    modules = {
     },
 }
 
