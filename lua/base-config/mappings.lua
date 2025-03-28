dd400ac92a3acbdff1d41010c4349f87dc28bf63
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-03-28 05:11:35 -0600

    refactor: change double quotes to single quotes

--- lua/base-config/mappings.lua
+++ lua/base-config/mappings.lua
@@ -2,17 +2,17 @@
 local set = vim.keymap.set
 
 -- Settings
-vim.g.mapleader = " " -- Mapping leader to space.
+vim.g.mapleader = ' ' -- Mapping leader to space.
 
-set({"i", "c"}, "<C-H>", "<C-W>") -- "ctrl + backspace" to delete whole word in insert mode.
+set({'i', 'c'}, '<C-H>', '<C-W>') -- 'ctrl + backspace' to delete whole word in insert mode.
 
-set("v", "K", ":m '<-2<CR>gv=gv") -- "K" in visual mode will move selection up
-set("v", "J", ":m '>+1<CR>gv=gv") -- "J" in visual mode will move selection down
+set('v', 'K', ":m '<-2<CR>gv=gv") -- 'K' in visual mode will move selection up
+set('v', 'J', ":m '>+1<CR>gv=gv") -- 'J' in visual mode will move selection down
 
 -- Use visual lines to move up and down
-set({"n", "v"}, "j", "gj")
-set({"n", "v"}, "k", "gk")
+set({'n', 'v'}, 'j', 'gj')
+set({'n', 'v'}, 'k', 'gk')
 
--- Yanking and pasting into the system clipboard
-set({"n", "v"}, "<leader>y", '"+y')
-set({"n", "v"}, "<leader>p", '"+p')
+-- Yanking and pasting into/from the system clipboard
+set({'n', 'v'}, "<leader>y", '"+y')
+set({'n', 'v'}, "<leader>p", '"+p')
