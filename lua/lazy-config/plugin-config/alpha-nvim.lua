86cb2d06e8df934cd9894d73a6ba648e9af6877a
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-03-28 04:32:44 -0600

    feat: add configuration for alpha-nvim

---
+++ lua/lazy-config/plugin-config/alpha-nvim.lua
@@ -0,0 +1,21 @@
+local alpha = require'alpha'
+local dashboard = require'alpha.themes.dashboard'
+dashboard.section.header.val = {
+    [[░▒▓███████▓▒░░▒▓████████▓▒░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓██████████████▓▒░  ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+    [[░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓██████▓▒░   ░▒▓██▓▒░  ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ]],
+}
+dashboard.section.buttons.val = {
+    dashboard.button('n', '  New file', '<cmd>ene <CR>'),
+    dashboard.button('SPC f f', '󰈞  Find file'),
+    dashboard.button('SPC f h', '󰊄  Find Help'),
+    -- dashboard.button('SPC f r', '  Frecency/MRU'),
+    dashboard.button('SPC f g', '󰈬  Find word'),
+    dashboard.button('c', '  Neovim Config', '<cmd>execute "e " .. stdpath("config") .. "/init.lua"<CR><cmd>execute "cd " .. stdpath("config")<CR>'),
+}
+dashboard.section.footer.val = 'Something better is coming...'
+alpha.setup(dashboard.config)
