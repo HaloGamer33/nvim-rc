b9489f01919335f6ff28e02032945e1425f992fb
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-03-31 17:03:05 -0600

    feat: add windwp/nvim-ts-autotag as a trial plugin

--- lua/lazy-config/plugin-list.lua
+++ lua/lazy-config/plugin-list.lua
@@ -28,6 +28,19 @@ Plugins = {
         ---@type ibl.config
         opts = {},
     },
+    {
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup({
+                opts = {
+                    -- Defaults
+                    enable_close = true, -- Auto close tags
+                    enable_rename = true, -- Auto rename pairs of tags
+                    enable_close_on_slash = false -- Auto close on trailing </
+                }
+            })
+        end
+    },
     -- End of on trial plugins
 
     -- "Aadv1k/gdoc.vim",                 -- Google Docs
