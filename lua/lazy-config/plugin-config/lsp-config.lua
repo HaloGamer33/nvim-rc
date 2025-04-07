66dd983cac0ee74fec5fc6206da58db8618d61ff
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-04-07 14:11:31 -0600

    style: remove comment, i didnt understand how completion worked

--- lua/lazy-config/plugin-config/lsp-config.lua
+++ lua/lazy-config/plugin-config/lsp-config.lua
@@ -30,7 +30,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup_handlers {
     function (server_name)
         require("lspconfig")[server_name].setup {
-            capabilities = capabilities -- I dont no autocomplete for code
+            capabilities = capabilities
         }
     end,
 }
