1cd4f4d5706c508a2d4900d583331eb449e64a32
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-04-07 03:09:50 -0600

    feat: add mini.move to replace the 'move code' mapping

--- lua/base-config/mappings.lua
+++ lua/base-config/mappings.lua
@@ -6,8 +6,8 @@ vim.g.mapleader = ' ' -- Mapping leader to space.
 
 set({'i', 'c'}, '<C-H>', '<C-W>') -- 'ctrl + backspace' to delete whole word in insert mode.
 
-set('v', 'K', ":m '<-2<CR>gv=gv") -- 'K' in visual mode will move selection up.
-set('v', 'J', ":m '>+1<CR>gv=gv") -- 'J' in visual mode will move selection down.
+-- set('v', 'K', ":m '<-2<CR>gv=gv") -- 'K' in visual mode will move selection up.
+-- set('v', 'J', ":m '>+1<CR>gv=gv") -- 'J' in visual mode will move selection down.
 
 -- Use visual lines to move up and down.
 set({'n', 'v'}, 'j', 'gj')
--- lua/lazy-config/plugin-list.lua
+++ lua/lazy-config/plugin-list.lua
@@ -1,6 +1,35 @@
 Plugins = {
     -- On trial
     -- Dashboard
+    -- lazy.nvim
+    {
+        'echasnovski/mini.move', version = false,
+        config = function ()
+            require('mini.move').setup({
+                -- Module mappings. Use `''` (empty string) to disable one.
+                mappings = {
+                    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+                    left = 'H',
+                    right = 'L',
+                    down = 'J',
+                    up = 'K',
+
+                    -- Move current line in Normal mode
+                    line_left = '',
+                    line_right = '',
+                    line_down = '',
+                    line_up = '',
+                },
+
+                -- Options which control moving behavior
+                options = {
+                    -- Automatically reindent selection during linewise vertical move
+                    reindent_linewise = true,
+                },
+            }
+            )
+        end
+    },
     {
         'goolord/alpha-nvim',
         dependencies = { 'echasnovski/mini.icons' },
