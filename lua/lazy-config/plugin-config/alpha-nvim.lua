ea0dd46090ed1140313a5beb9ac43a83d5c039ad
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-05-30 14:43:59 -0600

    feat: add telescope projects extension

--- lua/lazy-config/plugin-config/alpha-nvim.lua
+++ lua/lazy-config/plugin-config/alpha-nvim.lua
@@ -12,6 +12,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
     dashboard.button('n', '  New file', '<cmd>ene <CR>'),
     dashboard.button('SPC f f', '󰈞  Find file'),
+    dashboard.button('SPC f p', '  Find Projects'),
     dashboard.button('SPC f h', '󰊄  Find Help'),
     -- dashboard.button('SPC f r', '  Frecency/MRU'),
     dashboard.button('SPC f g', '󰈬  Find word'),
--- lua/lazy-config/plugin-config/telescope.lua
+++ lua/lazy-config/plugin-config/telescope.lua
@@ -27,6 +27,7 @@ require('telescope').setup {
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
+require('telescope').load_extension('projects')
 
 -- setting keybinds
 local builtin = require('telescope.builtin')
@@ -35,3 +36,5 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', extensions.file_browser.file_browser, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fp', extensions.projects.projects, {})
+-- vim.keymap.set('n', '<leader>fp', vim.cmd('Telescope projects'))
