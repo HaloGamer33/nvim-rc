906032c3e25c2e244e7b3678ef1d618d71738614
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-05-30 14:45:51 -0600

    feat: add custom parsers for javascript and typescript

--- lua/lazy-config/plugin-config/treesitter.lua
+++ lua/lazy-config/plugin-config/treesitter.lua
@@ -1,3 +1,4 @@
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = {},
@@ -20,6 +21,37 @@ require'nvim-treesitter.configs'.setup {
 
     modules = {
     },
+}
+
+parser_config.javascript = {
+    install_info = {
+        url = "~/Servers/tree-sitter-javascript", -- local path or git repo
+        files = {
+            "src/parser.c",
+            "src/scanner.c",
+        }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+        -- optional entries:
+        -- branch = "main", -- default branch in case of git repo if different from master
+        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    },
+    filetype = "js", -- if filetype does not match the parser name
+}
+
+parser_config.typescript = {
+    install_info = {
+        url = "~/Servers/tree-sitter-typescript", -- local path or git repo
+        location = "typescript",
+        files = {
+            "src/parser.c",
+            "src/scanner.c",
+        }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+        -- optional entries:
+        -- branch = "main", -- default branch in case of git repo if different from master
+        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    },
+    filetype = "ts", -- if filetype does not match the parser name
 }
 
 -- vim.opt.smartindent = false
