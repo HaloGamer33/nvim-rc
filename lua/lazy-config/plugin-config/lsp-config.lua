9fcd7dc4367f2fa9fe23516e624f0fb895477dcf
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-05-30 14:40:55 -0600

    refactor: rework lsp-config.lua to comply with new nvim lsp changes

--- lua/lazy-config/plugin-config/lsp-config.lua
+++ lua/lazy-config/plugin-config/lsp-config.lua
@@ -1,52 +1,44 @@
--- !!! IMPORTANT !!!
--- The in which this files are loaded is important.
--- 1. Mason
--- 2. Mason-Lspconfig
--- 3. Set Up of the servers via lspconfig
+require('lspconfig')
+require('mason').setup()
+require('mason-lspconfig').setup({
+    automatic_enable = true,
+    ensure_installed = {}
+})
 
---  ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
---  ╎ Mason Plugin ╎
---  └╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
-require("mason").setup()
-
-
---  ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
---  ╎ Mason-Lspconfig Plugin ╎
---  └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
-require("mason-lspconfig").setup {
-    automatic_installation = true,
-}
-
-
---  ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
---  ╎ Lspconfig Plugin ╎
---  └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
 -- Show full errors & warnings when hovering
 vim.o.updatetime = 750
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- This part replaces the necesity of setting up each individual server.
-require("mason-lspconfig").setup_handlers {
-    function (server_name)
-        require("lspconfig")[server_name].setup {
-            capabilities = capabilities
+vim.lsp.config('*', {
+    capabilities = capabilities
+})
+
+vim.lsp.config.ltex_plus = {
+    on_attach = function(client, bufnr)
+        require('ltex_extra').setup {
         }
     end,
+    settings = {
+        ltex = {
+            enabled = true,
+            language = 'en-US',
+        }
+    }
 }
 
 -- test
-local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "swift" },
+local swift_lsp = vim.api.nvim_create_augroup('swift_lsp', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'swift' },
     callback = function()
         local root_dir = vim.fs.dirname(vim.fs.find({
-            "Package.swift",
-            ".git",
+            'Package.swift',
+            '.git',
         }, { upward = true })[1])
         local client = vim.lsp.start({
-            name = "sourcekit-lsp",
-            cmd = { "sourcekit-lsp" },
+            name = 'sourcekit-lsp',
+            cmd = { 'sourcekit-lsp' },
             root_dir = root_dir,
         })
         vim.lsp.buf_attach_client(0, client)
