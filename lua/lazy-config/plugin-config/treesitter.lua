8216c0d7c3727334df8e8a9978a04241e593c251
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-08-20 02:26:29 -0600

    refactor: comment out custom treesitter parsers

--- lua/lazy-config/plugin-config/treesitter.lua
+++ lua/lazy-config/plugin-config/treesitter.lua
@@ -23,36 +23,36 @@ require'nvim-treesitter.configs'.setup {
     },
 }
 
-parser_config.javascript = {
-    install_info = {
-        url = "~/Servers/tree-sitter-javascript", -- local path or git repo
-        files = {
-            "src/parser.c",
-            "src/scanner.c",
-        }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-        -- optional entries:
-        -- branch = "main", -- default branch in case of git repo if different from master
-        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-    },
-    filetype = "js", -- if filetype does not match the parser name
-}
+-- parser_config.javascript = {
+--     install_info = {
+--         url = "~/Servers/tree-sitter-javascript", -- local path or git repo
+--         files = {
+--             "src/parser.c",
+--             "src/scanner.c",
+--         }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--         -- optional entries:
+--         -- branch = "main", -- default branch in case of git repo if different from master
+--         -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--         -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--     },
+--     filetype = "js", -- if filetype does not match the parser name
+-- }
 
-parser_config.typescript = {
-    install_info = {
-        url = "~/Servers/tree-sitter-typescript", -- local path or git repo
-        location = "typescript",
-        files = {
-            "src/parser.c",
-            "src/scanner.c",
-        }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-        -- optional entries:
-        -- branch = "main", -- default branch in case of git repo if different from master
-        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-    },
-    filetype = "ts", -- if filetype does not match the parser name
-}
+-- parser_config.typescript = {
+--     install_info = {
+--         url = "~/Servers/tree-sitter-typescript", -- local path or git repo
+--         location = "typescript",
+--         files = {
+--             "src/parser.c",
+--             "src/scanner.c",
+--         }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--         -- optional entries:
+--         -- branch = "main", -- default branch in case of git repo if different from master
+--         -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--         -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--     },
+--     filetype = "ts", -- if filetype does not match the parser name
+-- }
 
 -- vim.opt.smartindent = false
 require 'nvim-treesitter.install'.prefer_git = false
