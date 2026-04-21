6600a316d3c23adf8f1c1f009efbf6a4b1254b95
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-04-21 02:52:52 -0600

    feat(neo-tree): change mappings

--- lua/lazy-config/plugin-config/file-explorer.lua
+++ lua/lazy-config/plugin-config/file-explorer.lua
@@ -1,4 +1,12 @@
 -- For neo-tree.nvim
+require('neo-tree').setup({
+    window = {
+        mappings = {
+            ["."] = "toggle_hidden",
+            ["Z"] = "expand_all_nodes",
+        },
+    },
+})
 vim.keymap.set('n', '<C-n>', '<cmd>Neotree position=float reveal=true toggle=true<cr>')
 
 -- For nvim-tree.lua
