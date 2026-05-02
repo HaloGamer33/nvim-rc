2a2dde50a320647543bf2ea007f56b69ef81d312
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-01 23:24:53 -0600

    chore: order plugins alphabetically

--- lua/lazy-config/plugin-list.lua
+++ lua/lazy-config/plugin-list.lua
@@ -1,50 +1,50 @@
 Plugins = {
     "alec-gibson/nvim-tetris",           -- Tetris
     "barrett-ruth/live-server.nvim",     -- Live server
+    "catgoose/nvim-colorizer.lua",       -- Have a prview of colors on top of the text (#FF0000)
     "chentoast/marks.nvim",              -- Better marks
     "dstein64/vim-startuptime",          -- Startup Time
     "Eandrju/cellular-automaton.nvim",   -- Little visual games for the text buffer
     "fladson/vim-kitty",                 -- Syntax highlighting for kitty terminal config files
     "folke/twilight.nvim",               -- Focus on cursor dimming everything else
+    "johnfrankmorgan/whitespace.nvim",   -- Mark and remove trailling whitespace
     "junegunn/vim-easy-align",           -- Align based on characters
     "lambdalisue/suda.vim",              -- Write/Read as sudo
+    "lewis6991/gitsigns.nvim",           -- Signal git changes inside buffers
     "LudoPinelli/comment-box.nvim",      -- Comment boxes
     "mbbill/undotree",                   -- See undos in a pretty UI
-    "ThePrimeagen/vim-be-good",          -- ThePrimeagen goated game
-    "xiyaowong/transparent.nvim",        -- Transparent backgrounds
-    "smartpde/telescope-recent-files",   -- Obsidian companion plugin
-    "johnfrankmorgan/whitespace.nvim",   -- Mark and remove trailling whitespace
-    "lewis6991/gitsigns.nvim",           -- Signal git changes inside buffers
     "mks-h/treesitter-autoinstall.nvim", -- Auto install treesitter grammars
     "ohakutsu/socks-copypath.nvim",      -- Copy current file path
     "shortcuts/no-neck-pain.nvim",       -- Center text with the help of two buffers on the edges of the screen
-    "catgoose/nvim-colorizer.lua",       -- Have a prview of colors on top of the text (#FF0000)
+    "smartpde/telescope-recent-files",   -- Obsidian companion plugin
+    "ThePrimeagen/vim-be-good",          -- ThePrimeagen goated game
+    "xiyaowong/transparent.nvim",        -- Transparent backgrounds
 
     -- ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     -- ┃                      Color schemes                      ┃
     -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-    { "rebelot/kanagawa.nvim",        lazy = true },
+    { "EdenEast/nightfox.nvim",       lazy = true },
     { "folke/tokyonight.nvim",        lazy = true },
     { "kepano/flexoki-neovim",        lazy = true },
-    { "EdenEast/nightfox.nvim",       lazy = true },
-    { "miikanissi/modus-themes.nvim", lazy = true },
     { "maxmx03/fluoromachine.nvim",   lazy = true },
+    { "miikanissi/modus-themes.nvim", lazy = true },
     { "nyoom-engineering/oxocarbon.nvim", lazy = true },
+    { "rebelot/kanagawa.nvim",        lazy = true },
 
     -- ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     -- ┃                           LSP                           ┃
     -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
     "neovim/nvim-lspconfig",
-    "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason.nvim",
 
     -- ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     -- ┃                        Completion                       ┃
     -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-    "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
-    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/nvim-cmp",
     "rafamadriz/friendly-snippets",
+    "saadparwaiz1/cmp_luasnip",
     {
         "L3MON4D3/LuaSnip",
         version = "v2.*",
@@ -81,12 +81,12 @@ Plugins = {
     -- ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     -- ┃                      UI & Editor                        ┃
     -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-    { "folke/which-key.nvim",       event = "VeryLazy" },
-    { "folke/trouble.nvim",         cmd = "Trouble" },
-    { "windwp/nvim-autopairs",      event = "InsertEnter", config = true },
     { "echasnovski/mini.move",      version = false },
     { "folke/lazydev.nvim",         ft = "lua" },
+    { "folke/trouble.nvim",         cmd = "Trouble" },
+    { "folke/which-key.nvim",       event = "VeryLazy" },
     { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+    { "windwp/nvim-autopairs",      event = "InsertEnter", config = true },
     { "goolord/alpha-nvim",
         dependencies = { "echasnovski/mini.icons" },
     },
@@ -105,8 +105,8 @@ Plugins = {
         branch = "v3.x",
         lazy = false,
         dependencies = {
-            "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
         },
     },
