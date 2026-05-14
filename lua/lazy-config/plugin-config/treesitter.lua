6889f1c3c128fb10b248ed07d320bfc3789c2470
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-14 03:19:29 -0600

    feat: enable auto indentation with tree-sitter

--- lua/lazy-config/plugin-config/treesitter.lua
+++ lua/lazy-config/plugin-config/treesitter.lua
@@ -6,3 +6,11 @@ require("treesitter-autoinstall").setup({
     -- A list of *treesitter languages* to also enable regex highlighting for
     regex = {},
 })
+
+-- Auto enable indentation with TreeSitter on all files.
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
