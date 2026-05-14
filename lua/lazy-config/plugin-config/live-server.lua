119e3371eebed081db9bad456131147db54ae59f
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-14 16:12:40 -0600

    refactor: move single line setups to single file

--- lua/lazy-config/plugin-config/live-server.lua
+++
@@ -1,1 +0,0 @@
-vim.g.live_server = {}
---
+++ lua/lazy-config/plugin-config/single-line-setup.lua
@@ -0,0 +1,7 @@
+require("colorizer").setup()
+require("ibl").setup()
+require("ibl").setup()
+require("lastplace").setup()
+require("socks-copypath").setup()
+require("trouble").setup()
+vim.g.live_server = {}
